29cb9d468ec7546b02136cc49ec94a3a26ff2e47
noonespecial009 <noonespecial009@gmail.com>
2019-07-06 16:29:58 -0700

    add arcGIS point cloud

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <h2><router-link to="/index">Subscriptions</router-link></h2>
-        <router-link to="/create" class="btn btn-sm btn-primary">Add subscription</router-link>
+        <h2><router-link to="/index">geoHub</router-link></h2>
+        <router-link to="/create" class="btn btn-sm btn-primary">Add location</router-link>
 
         <router-view />
       </div>
